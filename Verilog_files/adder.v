af698ceabcce12ae80de4a77c0119c73570c3af0
my108 <112646892+my108@users.noreply.github.com>
2024-02-13 21:53:30 +0530

    Delete Verilog_files directory

--- Verilog_files/adder.v
+++
@@ -1,9 +0,0 @@
-module adder (
-    input a,b,cin,
-    output sum,carry
-);
-
-assign sum = a ^ b ^ cin;
-assign carry = (a & b) | (b & cin)  | (cin & a) ;
-
-endmodule
--- Verilog_files/div.v
+++
@@ -1,8 +0,0 @@
-module div(
-	input [10000000:0]A,
-	input [10000000:0]B,
-	output [10000000:0]C
-);
-
-assign C = A/B;
-endmodule
